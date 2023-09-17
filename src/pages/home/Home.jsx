2db31af302b3fe77f992ebc995e95f8f84de7875
abdusamadweb <abdusamadweb@gmail.com>
2023-09-17 14:46:15 +0500

    done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -23,7 +23,7 @@ const Home = ({ lang, setModal }) => {
             <iframe
                 style={{width: '100%', height: '400px'}}
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2979.4100272862183!2d60.73971711450085!3d41.69008338513977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41dfd4afe83d4e39%3A0x488112b8d5343d7a!2sBeruniy%20Tumani%20Khokimligi!5e0!3m2!1sen!2s!4v1657067626622!5m2!1sen!2s"
-                frameborder="0"
+                frameBorder="0"
             />
         </div>
     )
